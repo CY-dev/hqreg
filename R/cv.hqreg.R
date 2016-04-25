1a5125d321c57d22897e36ef008766da322ffd5c
Congrui Yi <congrui-yi@uiowa.edu>
2016-04-25 00:22:45 -0500

    Update cv.hqreg.R

--- R/cv.hqreg.R
+++ R/cv.hqreg.R
@@ -6,7 +6,7 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
   cv.args <- list(...)
   cv.args$lambda <- fit$lambda
   cv.args$gamma <- fit$gamma
-  loss.args <- list(method=fit$method, gamma=fit$gamma, tau=fit$tau, type.measure = type.measure)
+  measure.args <- list(method=fit$method, gamma=fit$gamma, tau=fit$tau, type.measure = type.measure)
   E <- matrix(NA, nrow=length(y), ncol=length(cv.args$lambda))
   n <- length(y)
   if(missing(fold.id)) fold.id <- ceiling(sample(1:n)/n*nfolds)
@@ -24,11 +24,11 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
     if (!("cluster" %in% class(cluster))) stop("Cluster is not of class 'cluster'; see ?makeCluster")
     parallel <- TRUE
     cat("Start parallel computing for cross-validation...")
-    clusterExport(cluster, c("fold.id", "X", "y", "cv.args", "loss.args"), 
+    clusterExport(cluster, c("fold.id", "X", "y", "cv.args", "measure.args"), 
                   envir=environment())
     clusterCall(cluster, function() require(hqreg))
     fold.results <- parLapply(cl = cluster, X = 1:nfolds, fun = cvf, XX = X, y = y, 
-                              fold.id = fold.id, cv.args = cv.args, loss.args = loss.args)
+                              fold.id = fold.id, cv.args = cv.args, measure.args = measure.args)
     stopCluster(cluster)
   }
   
@@ -38,7 +38,7 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
       fit.i <- fold.results[[i]]
     } else {
       if (trace) cat("Starting CV fold #",i,sep="","\n")
-      fit.i <- cvf(i, X, y, fold.id, cv.args, loss.args)
+      fit.i <- cvf(i, X, y, fold.id, cv.args, measure.args)
     }
     E[fold.id == i, 1:fit.i$nl] <- fit.i$pe
   }
@@ -59,7 +59,7 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
   structure(val, class="cv.hqreg")
 }
 
-cvf <- function(i, XX, y, fold.id, cv.args, loss.args) {
+cvf <- function(i, XX, y, fold.id, cv.args, measure.args) {
   cv.args$X <- XX[fold.id != i,,drop = FALSE]
   cv.args$y <- y[fold.id != i]
   X2 <- XX[fold.id == i,,drop = FALSE]
@@ -68,5 +68,5 @@ cvf <- function(i, XX, y, fold.id, cv.args, loss.args) {
   
   yhat <- matrix(predict(fit.i, X2), length(y2))
   
-  list(pe = loss.hqreg(y2, yhat, loss.args), nl = length(fit.i$lambda))
+  list(pe = measure.hqreg(y2, yhat, measure.args), nl = length(fit.i$lambda))
 }
