18da6d31d61e7b1ed8a31f93e7b51c4f92a20aec
Congrui Yi <congrui-yi@uiowa.edu>
2016-04-24 22:44:30 -0500

    Update hqreg.R

--- R/hqreg.R
+++ R/hqreg.R
@@ -1,5 +1,6 @@
-hqreg <- function (X, y, method = c("huber", "quantile", "ls"), gamma, tau = 0.5, alpha=1, nlambda=100, lambda.min = ifelse(nrow(X)>ncol(X), 0.001, 0.05), lambda, 
-		  preprocess = c("standardize", "rescale", "none"),  screen = c("ASR", "SR", "none"), max.iter = 10000, eps = 1e-7, dfmax = ncol(X)+1, penalty.factor=rep(1, ncol(X)), message = FALSE) {
+hqreg <- function (X, y, method = c("huber", "quantile", "ls"), gamma, tau = 0.5, alpha=1, nlambda=100, lambda.min = 0.05, lambda, 
+		  preprocess = c("standardize", "rescale", "none"),  screen = c("ASR", "SR", "none"), max.iter = 10000, eps = 1e-7, 
+		  dfmax = ncol(X)+1, penalty.factor=rep(1, ncol(X)), message = FALSE) {
   
   # Error checking
   method <- match.arg(method)
