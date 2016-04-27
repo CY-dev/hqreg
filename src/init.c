37e04620547153e58baadeba7a9c6e3cffca79bc
Congrui Yi <congrui-yi@uiowa.edu>
2016-04-27 00:37:12 -0500

    Update init.c

--- src/init.c
+++ src/init.c
@@ -9,50 +9,57 @@
 double sign(double x);
 
 // Fit the initial solutions for unpenalized features in elastic-net penalized models
-void init_huber(double *beta, double *beta_old, double *x, double *x2, double *y, double *r, double *pf, 
-		double *d1, double *d2, double gamma, double gi, double thresh, int max_iter, int n, int p) {
-  double v1, v2, pct, change, max_update, update; int i, j, jn;
-  for (j=0; j<p; j++) {
-    if (pf[j] == 0.0) { // unpenalized
-      // Calculate v1, v2
-      jn = j*n; v1 = 0.0; v2 = 0.0; pct = 0.0;
-      for (i=0; i<n; i++) {
-        v1 += x[jn+i]*d1[i];
-        v2 += x2[jn+i]*d2[i];
-        pct += d2[i];
-      }
-      v1 = v1/n; v2 = v2/n; pct = pct*gamma/n;
-      if (pct < 0.05 || pct < 1.0/n) {
-	// approximate v2 with a continuation technique
-        v2 = 0.0;
+void init_huber(double *beta, double *beta_old, int *iter, double *x, double *x2, 
+		double *y, double *r, double *pf, double *d1, double *d2, double gamma, 
+		double thresh, int n, int p, int max_iter) {
+  double gi = 1.0/gamma, v1, v2, pct, change, max_update, update; int i, j, jn;
+  while(iter[0]<max_iter) {
+    iter[0]++;
+    max_update = 0.0;
+    for (j=0; j<p; j++) {
+      if (pf[j] == 0.0) { // unpenalized
+        // Calculate v1, v2
+        jn = j*n; v1 = 0.0; v2 = 0.0; pct = 0.0;
         for (i=0; i<n; i++) {
-          if (d2[i]) {
-            v2 += x2[jn+i]*d2[i];
-          } else { // |r_i|>gamma
-            v2 += x2[jn+i]*d1[i]/r[i];
-          }
+          v1 += x[jn+i]*d1[i];
+          v2 += x2[jn+i]*d2[i];
+          pct += d2[i];
         }
-        v2 = v2/n;
-      }
-      // Update beta_j
-      beta[j] = beta_old[j] + v1/v2; 
-      // Update r, d1, d2 and compute candidate of max_update
-      change = beta[j]-beta_old[j];
-      if (fabs(change) > 1e-6) {
-        beta_old[j] = beta[j];
-        for (i=0; i<n; i++) {
-	  r[i] -= x[jn+i]*change;
-          if (fabs(r[i])>gamma) {
-            d1[i] = sign(r[i]);
-            d2[i] = 0.0;
-          } else {
-            d1[i] = r[i]*gi;
-            d2[i] = gi;
+        v1 = v1/n; v2 = v2/n; pct = pct*gamma/n;
+        if (pct < 0.05 || pct < 1.0/n) {
+	  // approximate v2 with a continuation technique
+          v2 = 0.0;
+          for (i=0; i<n; i++) {
+            if (d2[i]) {
+              v2 += x2[jn+i]*d2[i];
+            } else { // |r_i|>gamma
+              v2 += x2[jn+i]*d1[i]/r[i];
+            }
           }
+          v2 = v2/n;
         }
-        update = n*v2*change*change;
-        if (update>max_update) max_update = update;
+        // Update beta_j
+        beta[j] = beta_old[j] + v1/v2; 
+        // Update r, d1, d2 and compute candidate of max_update
+        change = beta[j]-beta_old[j];
+        if (fabs(change) > 1e-6) {
+          beta_old[j] = beta[j];
+          for (i=0; i<n; i++) {
+	    r[i] -= x[jn+i]*change;
+            if (fabs(r[i])>gamma) {
+              d1[i] = sign(r[i]);
+              d2[i] = 0.0;
+            } else {
+              d1[i] = r[i]*gi;
+              d2[i] = gi;
+            }
+          }
+          update = n*v2*change*change;
+          if (update>max_update) max_update = update;
+        }
       }
     }
+    // Check for convergence
+    if (max_update < thresh) break;
   }
 }
