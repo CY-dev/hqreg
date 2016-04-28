d212ebae13fc03f97a588299fb9647f53a85a432
Congrui Yi <congrui-yi@uiowa.edu>
2016-04-27 20:26:40 -0500

    Update init.c

--- src/init.c
+++ src/init.c
@@ -14,7 +14,7 @@ void init_huber(double *beta, double *beta_old, int *iter, double *x, double *x2
 		double *y, double *r, double *pf, double *d1, double *d2, double gamma, 
 		double thresh, int n, int p, int max_iter)
 {
-  double gi = 1.0/gamma, v1, v2, pct, change, max_update, update; int i, j, jn;
+  double gi = 1.0/gamma, v1, v2, pct, temp, change, max_update, update; int i, j, jn;
   while(iter[0]<max_iter) {
     iter[0]++;
     max_update = 0.0;
@@ -27,19 +27,15 @@ void init_huber(double *beta, double *beta_old, int *iter, double *x, double *x2
           v2 += x2[jn+i]*d2[i];
           pct += d2[i];
         }
-        v1 = v1/n; v2 = v2/n; pct = pct*gamma/n;
-        if (pct < 0.05 || pct < 1.0/n) {
+	pct = pct*gamma/n; // percentage of residuals with absolute values below gamma
+	if (pct < 0.05 || pct < 1.0/n) {
 	  // approximate v2 with a continuation technique
-          v2 = 0.0;
-          for (i=0; i<n; i++) {
-            if (d2[i]) {
-              v2 += x2[jn+i]*d2[i];
-            } else { // |r_i|>gamma
-              v2 += x2[jn+i]*d1[i]/r[i];
-            }
-          }
-          v2 = v2/n;
-        }
+	  for (i=0; i<n; i++) {
+	    temp = fabs(r[i]);
+	    if (temp > gamma) v2 += x2[jn+i]/temp; // d2[i] = 0
+	  }
+	}
+	v1 = v1/n; v2 = v2/n;
         // Update beta_j
         beta[j] = beta_old[j] + v1/v2; 
         // Update r, d1, d2 and compute candidate of max_update
@@ -86,19 +82,15 @@ void init_quantile(double *beta, double *beta_old, int *iter, double *x, double 
           v2 += x2[jn+i]*d2[i];
           pct += d2[i];
         }
-        v1 = v1/(2*n); v2 = v2/(2*n); pct = pct*gamma/n;
-        if (pct < 0.05 || pct < 1.0/n) {
+	pct = pct*gamma/n; // percentage of residuals with absolute values below gamma
+	if (pct < 0.05 || pct < 1.0/n) {
 	  // approximate v2 with a continuation technique
-          v2 = 0.0;
-          for (i=0; i<n; i++) {
-            if (d2[i]) {
-              v2 += x2[jn+i]*d2[i];
-            } else { // |r_i|>gamma
-              v2 += x2[jn+i]*(d1[i]-c)/r[i];
-            }
-          }
-          v2 = v2/(2*n);
-        }
+	  for (i=0; i<n; i++) {
+	    temp = fabs(r[i]);
+	    if (temp > gamma) v2 += x2[jn+i]/temp; // d2[i] = 0
+	  }
+	}
+	v1 = v1/(2.0*n); v2 = v2/(2.0*n);
         // Update beta_j
         beta[j] = beta_old[j] + v1/v2; 
         // Update r, d1, d2 and compute candidate of max_update
@@ -114,7 +106,7 @@ void init_quantile(double *beta, double *beta_old, int *iter, double *x, double 
               d2[i] = gi;
             }
           }
-          update = n*(v2*change*change + 2*fabs(v1*change));
+          update = 2.0*n*v2*change*change;
           if (update>max_update) max_update = update;
           beta_old[j] = beta[j];
         }
