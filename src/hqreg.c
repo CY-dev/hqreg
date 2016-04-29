50f27573aa8408c0fdcd330227685540997c28f6
Congrui Yi <congrui-yi@uiowa.edu>
2016-04-28 22:16:38 -0500

    Update hqreg.c

--- src/hqreg.c
+++ src/hqreg.c
@@ -436,7 +436,7 @@ static void sncd_quantile(double *beta, int *iter, double *lambda, int *saturate
               if (update > max_update) max_update = update;
               beta_old[j] = beta[lp+j];
             }
-            if(l == 99) Rprintf("pct = %3.2f, beta[%d] = %lf, s = %2.1f, mismatch = %d, change = %lf\n", pct, j, beta[lp+j], s[j], mismatch, change);
+            if(l == 50) Rprintf("pct = %3.2f, beta[%d] = %lf, s = %2.1f, mismatch = %d, change = %lf\n", pct, j, beta[lp+j], s[j], mismatch, change);
             if(!mismatch && update < thresh) break;
             }
           }
