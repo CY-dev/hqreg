4dc5fcf74e5a0b3aacd3b5b78acb09c7cf704226
Congrui Yi <congrui-yi@uiowa.edu>
2016-05-01 19:10:54 -0500

    Update predict.cv.hqreg.R

--- R/predict.cv.hqreg.R
+++ R/predict.cv.hqreg.R
@@ -8,10 +8,9 @@ predict.cv.hqreg <- function(object, X, lambda = c("lambda.1se","lambda.min"), t
 }
 
 coef.cv.hqreg <- function(object, lambda = c("lambda.1se","lambda.min"), ...) {
-  type = match.arg(type)
   if (is.character(lambda)) {
     lambda = match.arg(lambda)
     lambda = object[[lambda]]
   } else if(!is.numeric(lambda)) stop("Invalid lambda")
-  coef(object$fit, lambda = lambda, type = type, ...)
+  coef(object$fit, lambda = lambda, ...)
 }
