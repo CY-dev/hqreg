a192bde0a95c3ab8bd479a0f7c0c73215f063ad2
Congrui Yi <congrui-yi@uiowa.edu>
2016-05-08 11:41:21 -0500

    Update cv.hqreg.R

--- R/cv.hqreg.R
+++ R/cv.hqreg.R
@@ -16,9 +16,8 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
   if (ncores > 1) {
     max.cores <- detectCores()
     if (ncores > max.cores) {
-      cat("The number of cores specified (", ncores, ") is larger 
-                  than the number of avaiable cores (", max.cores, ") so",
-                  max.cores, "cores are used.", "\n")
+      cat("The number of cores specified (", ncores, ") is larger than 
+      the number of avaiable cores (", max.cores, "), so", max.cores, "cores are used.", "\n")
       ncores = max.cores
     }
     cluster <- makeCluster(ncores)
