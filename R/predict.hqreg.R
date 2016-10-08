8784df968da4668e7c4f6e1e2d07598727be925c
Congrui Yi <congrui-yi@uiowa.edu>
2016-10-08 11:30:53 -0500

    prediction for a fit with no intercept

--- R/predict.hqreg.R
+++ R/predict.hqreg.R
@@ -2,19 +2,29 @@ predict.hqreg <- function(object, X, lambda, type=c("response","coefficients","n
   type=match.arg(type)
   if (missing(X) && type == "response") stop("Need to supply 'X'")
   beta <- coef.hqreg(object, lambda = lambda, exact = exact)
+  intercept_added = (length(beta) > length(object$penalty.factor))
   if (type == "coefficients") return(beta)
-  if (is.matrix(beta)) {
-    b0 <- beta[1,]
-    b <- beta[-1,]
-  } else {
-    b0 <- beta[1]
-    b <- beta[-1]
+  if (intercept_added) {
+    if (is.matrix(beta)) {
+      b0 <- beta[1,]
+      b <- beta[-1,]
+    } else {
+      b0 <- beta[1]
+      b <- beta[-1]
+    }
+    if (type == "nvars") {
+      if (is.matrix(beta)) return(apply(b!=0, 2, sum))
+      else return(sum(b!=0))
+    }
+    if (type == "response") return(sweep(X %*% b, 2, b0, "+"))
+  } else { # only when object is fitted by hqreg_raw with intercept = FALSE
+    if (type == "nvars") {
+      if (is.matrix(beta)) return(apply(beta!=0, 2, sum))
+      else return(sum(beta!=0))
+    }
+    if (type == "response") return(X %*% beta)    
   }
-  if (type == "nvars") {
-    if (is.matrix(beta)) return(apply(b!=0, 2, sum))
-    else return(sum(b!=0))
-  }
-  if (type == "response") return(sweep(X %*% b, 2, b0, "+"))
+  NULL
 }
 
 coef.hqreg <- function(object, lambda, exact = FALSE, ...) {
@@ -41,5 +51,5 @@ coef.hqreg <- function(object, lambda, exact = FALSE, ...) {
     beta <- (1-weight)*object$beta[,left] + weight*object$beta[,right]
     if (length(lambda) > 1) colnames(beta) <- round(lambda,4)
   }
-  return(beta)
+  beta
 }
