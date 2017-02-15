d900c446fe4a25787b20fa1f5515e012faba46b9
Congrui Yi <cyi@expedia.com>
2017-02-14 22:30:48 -0800

    add support for hqreg_raw

--- R/cv.hqreg.R
+++ R/cv.hqreg.R
@@ -1,7 +1,12 @@
-cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c("deviance", "mse", "mae"), seed) {
-  type.measure = match.arg(type.measure)
+cv.hqreg <- function(X, y, ..., FUN = c("hqreg", "hqreg_raw"), ncores = 1, nfolds=10, fold.id, type.measure = c("deviance", "mse", "mae"), seed) {
+  FUN <- get(match.arg(FUN))
+#  FUN <- get(FUN)
+  type.measure <- match.arg(type.measure)
+  n <- length(y)
   if (!missing(seed)) set.seed(seed)
-  fit <- hqreg(X, y, ...)
+  if(missing(fold.id)) fold.id <- ceiling(sample(1:n)/n*nfolds)
+
+  fit <- FUN(X, y, ...)
   cv.args <- list(...)
   cv.args$lambda <- fit$lambda
   cv.args$alpha <- fit$alpha
@@ -9,15 +14,13 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
   cv.args$tau <- fit$tau
   measure.args <- list(method=fit$method, gamma=fit$gamma, tau=fit$tau, type.measure = type.measure)
   E <- matrix(NA, nrow=length(y), ncol=length(cv.args$lambda))
-  n <- length(y)
-  if(missing(fold.id)) fold.id <- ceiling(sample(1:n)/n*nfolds)
   
   parallel <- FALSE
   if (ncores > 1) {
     max.cores <- detectCores()
     if (ncores > max.cores) {
       cat("The number of cores specified (", ncores, ") is larger than 
-      the number of avaiable cores (", max.cores, "), so", max.cores, "cores are used.", "\n")
+          the number of avaiable cores (", max.cores, "), so", max.cores, "cores are used.", "\n")
       ncores = max.cores
     }
     cluster <- makeCluster(ncores)
@@ -26,7 +29,7 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
     cat("Start parallel computing for cross-validation...")
     clusterExport(cluster, c("fold.id", "X", "y", "cv.args", "measure.args"), 
                   envir=environment())
-    clusterCall(cluster, function() require(hqreg))
+    clusterCall(cluster, function() require(FUN))
     fold.results <- parLapply(cl = cluster, X = 1:nfolds, fun = cvf, XX = X, y = y, 
                               fold.id = fold.id, cv.args = cv.args, measure.args = measure.args)
     stopCluster(cluster)
@@ -38,7 +41,7 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
       fit.i <- fold.results[[i]]
     } else {
       cat("CV fold #",i,sep="","\n")
-      fit.i <- cvf(i, X, y, fold.id, cv.args, measure.args)
+      fit.i <- cvf(i, X, y, fold.id, cv.args, measure.args, FUN)
     }
     E[fold.id == i, 1:fit.i$nl] <- fit.i$pe
   }
@@ -57,16 +60,14 @@ cv.hqreg <- function(X, y, ..., ncores = 1, nfolds=10, fold.id, type.measure = c
   val <- list(cve = cve, cvse = cvse, type.measure = type.measure, lambda = lambda, fit = fit, 
               lambda.1se = lambda[index.1se], lambda.min = lambda[index.min])
   structure(val, class="cv.hqreg")
-}
+  }
 
-cvf <- function(i, XX, y, fold.id, cv.args, measure.args) {
+cvf <- function(i, XX, y, fold.id, cv.args, measure.args, FUN) {
   cv.args$X <- XX[fold.id != i,,drop = FALSE]
   cv.args$y <- y[fold.id != i]
   X2 <- XX[fold.id == i,,drop = FALSE]
-  y2 <- y[fold.id == i]  
-  fit.i <- do.call("hqreg", cv.args)
-  
-  yhat <- matrix(predict(fit.i, X2), length(y2))
-  
+  y2 <- y[fold.id == i]
+  fit.i <- do.call(FUN, cv.args)
+  yhat <- matrix(predict.hqreg(fit.i, X2), length(y2))
   list(pe = measure.hqreg(y2, yhat, measure.args), nl = length(fit.i$lambda))
 }
