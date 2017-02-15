4d1b879002fbc552b8943bf4001317bc23c58e5b
Congrui Yi <cyi@expedia.com>
2017-02-14 23:47:27 -0800

    add support for hqreg_raw

--- R/cv.hqreg.R
+++ R/cv.hqreg.R
@@ -1,5 +1,6 @@
 cv.hqreg <- function(X, y, ..., FUN = c("hqreg", "hqreg_raw"), ncores = 1, nfolds=10, fold.id, type.measure = c("deviance", "mse", "mae"), seed) {
-  FUN <- hqreg::(match.arg(FUN))
+  FUN <- match.arg(FUN)
+  FUN <- hqreg::FUN
 #  FUN <- get(FUN)
   type.measure <- match.arg(type.measure)
   n <- length(y)
